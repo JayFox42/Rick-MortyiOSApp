f47cc3b54a363037a8cbb1f8a0a1a5d0dec3486e
Jason <jasonfox42@hotmail.com>
2024-05-06 22:33:11 +0100

    Added docstrings and API related files

---
+++ Rick & Morty/APIClient/RMEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  RMEndpoint.swift
+//  Rick & Morty
+//
+//  Created by Jason Fox on 06/05/2024.
+//
+
+import Foundation
---
+++ Rick & Morty/APIClient/RMRequest.swift
@@ -0,0 +1,8 @@
+//
+//  RMRequest.swift
+//  Rick & Morty
+//
+//  Created by Jason Fox on 06/05/2024.
+//
+
+import Foundation
